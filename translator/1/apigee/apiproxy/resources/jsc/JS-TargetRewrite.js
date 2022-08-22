b20ce31f7ae5d6a4ebc3e1624b7c3aeae469620d
rakesh-talanki <109761030+rakesh-talanki@users.noreply.github.com>
2022-08-22 10:59:42 -0400

    Apigee Proxy

---
+++ translator/1/apigee/apiproxy/resources/jsc/JS-TargetRewrite.js
@@ -0,0 +1,5 @@
+context.setVariable("target.copy.pathsuffix", false); 
+
+//var targetURL = "us-central1-aiplatform.googleapis.com/v1/projects/" + PROJECT_ID + "/locations/us-central1/endpoints/" + ENDPOINT_ID + ":predict";
+
+//context.setVariable("target.url",targetURL);
